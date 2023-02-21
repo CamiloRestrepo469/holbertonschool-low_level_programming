4a62492a46be48c4848f8795643717119f496321
CamiloRestrepo469 <rsgermancamilo@gmail.com>
2023-02-21 12:46:39 -0800

    combi number

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of single-digit numbers.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int numero;
+
+	for (numero = 0; numero <= 9; numero++)
+	{
+		putchar((numero % 10) + '0');
+		if (numero == 9)
+			continue;
+		
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
