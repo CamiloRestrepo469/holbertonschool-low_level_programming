cff7e0584a009f4e06b732f686657bd2bcb84d30
CamiloRestrepo469 <rsgermancamilo@gmail.com>
2023-02-23 19:41:46 -0800

    8-print_square.c

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,7 +1,8 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
-int _putchar(char c);
+int putchar(char c);
+int _putchar(int c);
 
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -23,5 +24,7 @@ void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
+void print_square(int size)
+void print_square(int size);
 
 #endif
