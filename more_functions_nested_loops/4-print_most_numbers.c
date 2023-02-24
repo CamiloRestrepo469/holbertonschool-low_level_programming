6c414f03e8287d0f08aff3c46a835741b9b81ec4
CamiloRestrepo469 <rsgermancamilo@gmail.com>
2023-02-23 17:47:41 -0800

    4-print_most_numbers

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,18 +1,21 @@
 #include "main.h"
 /**
-  * print_numbers - Print the numbers since 0 up to 9
-  *
-  * Return: The numbers since 0 up to 9
-  */
-void print_numbers(void)
+ * print_most_numbers - entry point
+ *
+ * Description: Prints the numbers with _putchar
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
 {
-int x;
-for (x = 0; x <= 9; x++)
-{
-if (x != 2 && x != 4)
-{
-_putchar(x + '0');
-}
-}
-_putchar('\n');
+	int number;
+
+	for (number = '0'; number <= '9'; number++)
+	{
+		if (number == '2' || number == '4')
+			;
+		else
+			_putchar(number);
+	}
+	_putchar('\n');
 }
