9b0f22497c6e677077d0094a15b4ec5dca91b987
CamiloRestrepo469 <rsgermancamilo@gmail.com>
2023-02-28 08:01:40 -0800

    add a new search feature

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - a function that returns
+ *  the length of a string
+ *  @s: save values add
+ *
+ *  Return: 0.
+ */
+
+int _strlen(char *s)
+{
+char i;
+i = strlen(s);
+return (i);
+}
