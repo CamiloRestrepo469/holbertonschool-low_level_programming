40c0e582fd89e57f24f02562af6c6e8c9b788b78
CamiloRestrepo469 <rsgermancamilo@gmail.com>
2023-02-28 11:46:09 -0800

    add a new file

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * rev_string - a function that reverses a string
+ * @s: save value add
+ *
+ * Return: 0.
+ */
+void rev_string(char *s)
+{
+int l = 0, i = 0;
+char t;
+while (s[i++])
+l++;
+for (i = l - 1; i >= l / 2; i--)
+{
+t = s[i];
+s[i] = s[l - i - 1];
+s[l - i - 1] = t;
+}
+}
