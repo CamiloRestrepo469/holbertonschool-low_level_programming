7e9c99be81c2787fdab397a87bbf2e77adb25189
Holberton <holberton@Holbertons-iMac.local>
2023-03-02 15:04:23 -0500

    fix problem of stract

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -6,19 +6,20 @@
  * including the terminating
  * null byte, to the end of the string pointed to by @dest.
  * @dest: A pointer to the string to be concatenated upon.
- * @src: The source string to be appended to @dest.
+ * @src: The source string  appended to @dest.
  *
  * Return: A pointer to the destination string @dest.
  */
 char *_strcat(char *dest, char *src)
 {
-	int i = 0, dest_len = 0;
-
-	while (dest[i++])
-		dest_len++;
-
-	for (i = 0; src[i]; i++)
-		dest[dest_len++] = src[i];
-
-	return (dest);
+int i, j;
+for (i = 0; dest[i] != '\0'; i++)
+;
+for (j = 0; src[j] != '\0'; j++)
+{
+dest[i] = dest[j];
+i++;
+}
+dest[i] = '\0';
+return (dest);
 }
