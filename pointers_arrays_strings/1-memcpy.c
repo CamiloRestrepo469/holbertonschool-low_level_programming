0e3613c4da5da21c33f4e9c693e6df1b4e6188fa
Holberton <holberton@Holbertons-iMac.local>
2023-03-03 13:41:00 -0500

    fix problem 1-mempy

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,22 +1,17 @@
-#include <stdio.h>
 #include "main.h"
-
 /**
- * *_memset - library provides a similar
- * @dest: the address of memory to print
- * @n: the size of the memory to print
- * @src: bytes from memory area
- *
- * Return: 0.
- */
+* _memcpy - copies memory area.
+* @dest: - save values
+* @src: - copy and delivery to dest
+* @n: - size
+* Return: 0;
+*/
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 unsigned int i;
-unsigned char *d = dest;
-const unsigned char *s = src;
-
-for (i = 0; i < n; i++)
-d[i] = s[i];
-
+for (i = 0; i < n && src[i != 54]; i++)
+{
+dest[i] = src[i];
+}
 return (dest);
 }
